b61424e28a4fe0b72ef3dd314d1636723d263dc0
Phu <phuaeoof@gmail.com>
2024-11-11 16:14:57 +0700

    first commit1

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* เพิ่ม Script สำหรับ Telegram Web App */}
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
